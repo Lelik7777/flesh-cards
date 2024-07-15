d1defdf666626f93ce2661689d5ba36f8c7f57db
Lelik Jan <21alex7777@gmail.com>
2024-07-15 19:53:42 +0300

    feat:add directory Table with files

---
+++ src/components/ui/Table/Table.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Table } from './Table'
+
+const meta = {
+  component: Table,
+  tags: ['autodocs'],
+  title: 'Components/Table',
+} satisfies Meta<typeof Table>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
---
+++ src/components/ui/Table/Table.tsx
@@ -0,0 +1,7 @@
+import styles from './Table.module.scss'
+
+type Props = {}
+
+export const Table = ({}: Props) => {
+  return <></>
+}

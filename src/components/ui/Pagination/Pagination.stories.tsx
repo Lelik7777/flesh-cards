4eec78530ad821a14faa1a45906977be6866c9e1
Lelik Jan <21alex7777@gmail.com>
2024-07-20 21:45:35 +0300

    feat(pagination) add component with styles and stories,add hook usePagination

--- src/components/ui/Pagination/Pagination.stories.tsx
+++ src/components/ui/Pagination/Pagination.stories.tsx
@@ -1,9 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { useState } from 'react'
+
 import { Pagination } from './Pagination'
 
 const meta = {
+  args: {
+    currentPage: 1,
+    onChangePage: () => {},
+    pageSize: 1,
+    totalCount: 10,
+  },
   component: Pagination,
+
   tags: ['autodocs'],
   title: 'Components/Pagination',
 } satisfies Meta<typeof Pagination>
@@ -12,5 +21,19 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  render: args => {
+    const [page, setPage] = useState(1)
+    const handlePageChange = (num: number) => {
+      setPage(num)
+    }
+
+    return (
+      <Pagination
+        currentPage={page}
+        onChangePage={handlePageChange}
+        pageSize={args.pageSize}
+        totalCount={args.totalCount}
+      />
+    )
+  },
 }

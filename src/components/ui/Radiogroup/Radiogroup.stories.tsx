6283ae54f96961825913202a0ba07ee8dd433a0a
Lelik Jan <21alex7777@gmail.com>
2024-07-24 21:42:53 +0300

    feat(radio):add logic,styles and stories for radiogroup component

--- src/components/ui/Radiogroup/Radiogroup.stories.tsx
+++ src/components/ui/Radiogroup/Radiogroup.stories.tsx
@@ -1,16 +1,51 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Radiogroup } from './Radiogroup'
-
+//import { RadioGroupItem, RadioItem, Radiogroup } from './Radiogroup'
+import { Radio, RadioGroup } from './Radiogroup'
 const meta = {
-  component: Radiogroup,
+  component: RadioGroup,
   tags: ['autodocs'],
-  title: 'Components/Radiogroup',
-} satisfies Meta<typeof Radiogroup>
+  title: 'Components/RadioGroup',
+} satisfies Meta<typeof RadioGroup>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
+type RadioItem = {
+  id: string
+  label: string
+  value: string
+}
+const items: RadioItem[] = [
+  { id: '0', label: 'radiogroup', value: 'radiogroup0' },
+  { id: '1', label: 'radiogroup', value: 'radiogroup1' },
+  { id: '2', label: 'radiogroup', value: 'radiogroup2' },
+  { id: '3', label: 'radiogroup', value: 'radiogroup3' },
+  { id: '4', label: 'radiogroup', value: 'radiogroup4' },
+]
+
 export const Default: Story = {
-  args: {},
+  render: () => {
+    return (
+      <RadioGroup>
+        {items.map(item => (
+          <Radio id={item.id} key={item.id} label={item.label} value={item.value} />
+        ))}
+      </RadioGroup>
+    )
+  },
+}
+export const DisabledRadioGroup: Story = {
+  args: {
+    disabled: true,
+  },
+  render: args => {
+    return (
+      <RadioGroup {...args}>
+        {items.map(item => (
+          <Radio id={item.id} key={item.id} label={item.label} value={item.value} />
+        ))}
+      </RadioGroup>
+    )
+  },
 }
--- src/components/ui/Radiogroup/Radiogroup.tsx
+++ src/components/ui/Radiogroup/Radiogroup.tsx
@@ -1,7 +1,61 @@
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
+
+import * as RadioGroupRadix from '@radix-ui/react-radio-group'
+import clsx from 'clsx'
+
 import styles from './Radiogroup.module.scss'
 
-type Props = {}
+import { Typography } from '../Typography'
 
-export const Radiogroup = ({}: Props) => {
-  return <></>
-}
+type RadioGroupProps = {
+  children?: ReactNode
+  defaultValue?: string
+  disabled?: boolean
+} & ComponentPropsWithoutRef<typeof RadioGroupRadix.Root>
+
+export const RadioGroup = forwardRef<ElementRef<typeof RadioGroupRadix.Root>, RadioGroupProps>(
+  ({ children, defaultValue, disabled }: RadioGroupProps, ref) => {
+    const classNames = {
+      root: styles.root,
+    }
+
+    return (
+      <RadioGroupRadix.Root
+        aria-label={'View density'}
+        className={clsx(classNames.root)}
+        defaultValue={defaultValue}
+        disabled={disabled}
+        ref={ref}
+      >
+        {children}
+      </RadioGroupRadix.Root>
+    )
+  }
+)
+
+type RadioProps = {
+  id?: string
+  label?: string
+  value?: string
+} & ComponentPropsWithoutRef<typeof RadioGroupRadix.Item>
+
+export const Radio = forwardRef<ElementRef<typeof RadioGroupRadix.Item>, RadioProps>(
+  ({ id, label, value }: RadioProps, ref) => {
+    const classNames = {
+      indicator: styles.indicator,
+      item: styles.item,
+      label: styles.label,
+    }
+
+    return (
+      <div className={styles.container}>
+        <RadioGroupRadix.Item className={clsx(classNames.item)} id={id} ref={ref} value={value}>
+          <RadioGroupRadix.Indicator className={clsx(classNames.indicator)} />
+        </RadioGroupRadix.Item>
+        <Typography as={'label'} className={clsx(classNames.label)} htmlFor={id} variant={'body2'}>
+          {label}
+        </Typography>
+      </div>
+    )
+  }
+)

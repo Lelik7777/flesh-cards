f6ad7aba209cfe773ec4fba71b10a655a975d9b8
Lelik Jan <21alex7777@gmail.com>
2024-08-06 17:31:00 +0300

    feat(SignIn): add SignIn component and stories
    - Add SignIn component with styles and stories
    - Create Storybook story for SignIn with a centered wrapper for better visualization
    - Utilize shared card styles with SCSS mixin"
    - Change style for Button .link

--- src/components/auth/SignIn/SignIn.stories.tsx
+++ src/components/auth/SignIn/SignIn.stories.tsx
@@ -1,16 +1,39 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import React from 'react'
+
 import { SignIn } from './SignIn'
 
-const meta = {
+const meta: Meta<typeof SignIn> = {
   component: SignIn,
   tags: ['autodocs'],
   title: 'Components/SignIn',
-} satisfies Meta<typeof SignIn>
+}
 
 export default meta
+
 type Story = StoryObj<typeof meta>
+
+const Wrapper = ({ children }: { children: React.ReactNode }) => (
+  <div
+    style={{
+      alignItems: 'center',
+      backgroundColor: 'var(--color-dark-900)',
+      display: 'flex',
+      justifyContent: 'center',
+      minHeight: '100vh',
+      padding: '20px',
+    }}
+  >
+    {children}
+  </div>
+)
 
 export const Default: Story = {
   args: {},
+  render: args => (
+    <Wrapper>
+      <SignIn {...args} />
+    </Wrapper>
+  ),
 }
--- src/components/auth/SignIn/SignIn.tsx
+++ src/components/auth/SignIn/SignIn.tsx
@@ -1,7 +1,38 @@
+import { Card, Checkbox, TextField, Typography } from '@/components/ui'
+import { Button } from '@/components/ui/Button'
+import clsx from 'clsx'
+
 import styles from './SignIn.module.scss'
 
-type Props = {}
+type SignInProps = {}
 
-export const SignIn = ({}: Props) => {
-  return <></>
+export const SignIn = ({}: SignInProps) => {
+  const classes = {
+    card: styles.card,
+    container: styles.container,
+    forgot: styles.forgot,
+    form: styles.form,
+    signUp: styles.signUp,
+  } as const
+
+  return (
+    <Card className={clsx(classes.card)}>
+      <Typography variant={'h1'}>Sign In</Typography>
+      <div className={clsx(classes.form)}>
+        <div className={clsx(classes.container)}>
+          <TextField label={'Email'} />
+          <TextField label={'Password'} type={'password'} />
+        </div>
+        <Checkbox label={'Remember me'} />
+        <Typography className={clsx(classes.forgot)} variant={'body2'}>
+          Forgot password?
+        </Typography>
+        <Button fullWidth>Sign In</Button>
+      </div>
+      <div className={clsx(classes.signUp)}>
+        <Typography variant={'body2'}>Don't have an account?</Typography>
+        <Button variant={'link'}>Sign Up</Button>
+      </div>
+    </Card>
+  )
 }
